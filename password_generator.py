37783075e63430b1352e805afe3e30ff72b6509c
Foxx-etc <charlieadya2007@gmail.com>
2026-01-27 21:08:22 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,27 +1,24 @@
 import random as r
 
-lower = []
-lower.extend("qwertyuiopasdfghjklzxcvbnm")
-
-upper = []
-upper.extend("QWERTYUIOPASDFGHJKLZXCVBNM")
+# we would use this variable to convert all the letters to upper case by str().upper()
+alpha = []
+alpha.extend("qwertyuiopasdfghjklzxcvbnm")
 
 symbols = []
-symbols.extend("~!@#$%^&*()_+-[]{}:;',.><?/")
+symbols.extend("~!@#$%^&*(+-[}:;<?/")
 
 num = []
 num.extend("1234567890")
 
-
+# validation
 while True:
     try:
         lenOfPass = int(input("Lenght Of Password : "))
         break
     except:
-        print("Invalid Input! Enter Number...")
+        print("Invalid Input! Enter Number...\n")
 
 complexity = input("Enter The Complexity Of The Password (E)asy, (M)edium, (H)ard : ").upper()
-
 
 
 # Easy : only including charachters from 2 variables - lower, upper
@@ -29,11 +26,11 @@ if complexity == 'E':
     result = []
     r.shuffle(result)
     while len(result) <= lenOfPass:
-        result.append(r.choice(lower))
-        result.append(r.choice(upper))
+        result.append(r.choice(alpha))
+        result.append(r.choice(str(alpha).upper()))
     while len(result) != lenOfPass:
         result.pop()
-
+    print(f'Your Password : { ''.join(result) }')
 
 
 # Medium : only including charachters from 3 variables - lower, upper, symbols
@@ -41,12 +38,12 @@ elif complexity == 'M':
     result = []
     r.shuffle(result)
     while len(result) <= lenOfPass:
-        result.append(r.choice(lower))
-        result.append(r.choice(upper))
+        result.append(r.choice(alpha))
+        result.append(r.choice(str(alpha).upper()))
         result.append(r.choice(symbols))
     while len(result) != lenOfPass:
         result.pop()
-
+    print(f'Your Password : { ''.join(result) }')
 
 
 # Hard : only including charachters from all 4 variables - lower, upper, symbols, num
@@ -54,14 +51,12 @@ elif complexity == 'H':
     result = []
     r.shuffle(result)
     while len(result) <= lenOfPass:
-        result.append(r.choice(lower))
-        result.append(r.choice(upper))
+        result.append(r.choice(alpha))
+        result.append(r.choice(str(alpha).upper()))
         result.append(r.choice(symbols))
         result.append(r.choice(num))
     while len(result) != lenOfPass:
         result.pop()
-
-
-print("".join(result))
-
-
+    print(f'Your Password : { ''.join(result) }')
+else:
+    print("Invalid Input! Try Again")
