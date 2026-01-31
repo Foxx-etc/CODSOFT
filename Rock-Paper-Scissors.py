2b05c936926bbeb0272f9c0914555362720f0803
Foxx-etc <charlieadya2007@gmail.com>
2026-01-31 19:07:21 +0530

    Update Rock-Paper-Scissors.py

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,4 +1,6 @@
 import random as r
+import os
+import time
 
 #name of the player
 name = input("Enter Your Name : ")
@@ -6,34 +8,48 @@ name = input("Enter Your Name : ")
 choose = ['ROCK', 'PAPER', 'SCISSOR']
 
 
+
 def play():
-    for i in range(5):
+    os.system("clear")
+    print(f"WELCOME {name}!")
+    valid = []
+
+
+    while len(valid) != 5:
         print("----------------------------------------")
         user = input("Enter ROCK, PAPER, SCISSOR : ").strip().upper()
         computer = r.choice(choose)
         print("----------------------------------------")
 
-        if user == computer:
+
+        if user == computer:               
+                time.sleep(1)
                 print(f'{name}: {user}')
                 print(f'computer: {computer}\n')
                 print("IT's A TIE!\n")
 
 
         elif user == 'ROCK' and computer == 'SCISSOR' or user == 'S' and computer == 'SCISSOR':
+                time.sleep(1)
                 print(f'{name}: {user}')
                 print(f'Computer: {computer}\n')
+                valid.append(1)
                 print(f'{name.upper()} , WON!' if user == 'R' else f'COMPUTER WON!\n')
 
 
         elif user == 'SCISSOR' and computer == 'PAPER' or user == 'PAPER' and computer == 'SCISSORP':
+                time.sleep(1)
                 print(f'{name}: {user}')
                 print(f'Computer: {computer}\n')
+                valid.append(1)
                 print(f'{name.upper()} , WON!' if user == 'R' else f'COMPUTER WON!\n')
 
 
         elif user == 'PAPER' and computer == 'ROCK' or user == 'ROCK' and computer == 'PAPER':
+                time.sleep(1)
                 print(f'{name}: {user}')
                 print(f'Computer: {computer}\n')
+                valid.append(1)
                 print(f'{name.upper()} , WON!' if user == 'R' else f'COMPUTER WON!\n')
 
 
